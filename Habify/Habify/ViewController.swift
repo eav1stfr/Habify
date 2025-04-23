fb67ad635fd332403b6de40bdb776002f6980725
eav1stfr <eavst123@gmail.com>
2025-04-23 13:50:10 +0500

    finish the introduction page, start working on the login page

--- Habify/Habify/ViewController.swift
+++ Habify/Habify/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Habify
-//
-//  Created by Александр Эм on 22.04.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
--- Habify/Habify/Views/Introduction/Introduction.swift
+++ Habify/Habify/Views/Introduction/Introduction.swift
@@ -1,6 +1,35 @@
 import UIKit
 
 final class IntroductionView: UIView {
+    
+    enum Constants {
+        enum Colors {
+            static let mainBlue = UIColor(named: "ThemeColor")!
+            static let mainBackground = UIColor(named: "BackgroundColor")!
+        }
+        
+        enum Color {
+            case mainBlue
+            case mainBackgroud
+            
+            var color: UIColor {
+                switch self {
+                case .mainBlue: return UIColor(named: "ThemeColor")!
+                case .mainBackgroud: return UIColor(named: "BackgroundColor")!
+                }
+            }
+        }
+        
+        enum Texts {
+            static let deviz = "Stay focused, stau committed,\n and let Habify guide your personal growth"
+            static let appName = "HABIFY"
+            static let loginText = "Log In"
+            static let createAccountText = "Create an Account"
+            static let termsText = "Terms of Service"
+            static let privacyText = "Privacy Policy"
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -9,26 +38,162 @@ final class IntroductionView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     var presenter: PresenterProtocol?
+    
+    private lazy var termsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isUserInteractionEnabled = true
+        label.font = UIFont.systemFont(ofSize: 15, weight: .light)
+        let atrtibutedText = NSMutableAttributedString(string: Constants.Texts.termsText)
+        atrtibutedText.addAttributes([
+            .underlineStyle: NSUnderlineStyle.single.rawValue,
+            .foregroundColor: Constants.Color.mainBlue.color],
+                                     range: NSRange(location: 0, length: Constants.Texts.termsText.count))
+        label.attributedText = atrtibutedText
+        label.tag = 0
+        let tap = UITapGestureRecognizer(target: self, action: #selector(termPressed))
+        label.addGestureRecognizer(tap)
+        return label
+    }()
+    
+    private lazy var privacyLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isUserInteractionEnabled = true
+        label.font = UIFont.systemFont(ofSize: 15, weight: .light)
+        let atrtibutedText = NSMutableAttributedString(string: Constants.Texts.privacyText)
+        atrtibutedText.addAttributes([
+            .underlineStyle: NSUnderlineStyle.single.rawValue,
+            .foregroundColor: Constants.Color.mainBlue.color],
+                                     range: NSRange(location: 0, length: Constants.Texts.privacyText.count))
+        label.attributedText = atrtibutedText
+        label.tag = 1
+        let tap = UITapGestureRecognizer(target: self, action: #selector(privacyPressed))
+        label.addGestureRecognizer(tap)
+        return label
+    }()
+    
+    private let delimiter: UILabel = {
+        let label = UILabel()
+        label.text = "|"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Constants.Colors.mainBlue
+        label.font = UIFont.systemFont(ofSize: 15)
+        return label
+    }()
+    
+    
+    @objc
+    private func termPressed() {
+        guard let url = URL(string: "https://dictionary.cambridge.org/dictionary/english/privacy") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @objc
+    private func privacyPressed() {
+        guard let url = URL(string: "https://arc.net/privacy") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    private let citation: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = Constants.Texts.deviz
+        label.font = UIFont(name: "Futura", size: 15)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        
+        return label
+    }()
+    
+    private lazy var mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constants.Texts.appName
+        label.textColor = Constants.Colors.mainBlue
+        label.font = UIFont.systemFont(ofSize: 40, weight: .heavy)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let tempLogo: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "bird")
+        image.tintColor = Constants.Colors.mainBlue
+        image.clipsToBounds = true
+        image.backgroundColor = Constants.Colors.mainBackground
+        return image
+    }()
     
     private lazy var loginButton: LoginRegistrationButton = {
         let button = LoginRegistrationButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.buttonLabel = "Log In"
-        button.buttonColor = UIColor(named: "ThemeColor")!
+        button.buttonLabel = Constants.Texts.loginText
+        button.buttonColor = Constants.Colors.mainBlue
+        button.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
+        button.tag = 1
+        return button
+    }()
+    
+    private lazy var registrationButton: LoginRegistrationButton = {
+        let button = LoginRegistrationButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.buttonLabel = Constants.Texts.createAccountText
+        button.buttonColor = Constants.Color.mainBlue.color
+        button.tag = 2
         button.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
         return button
     }()
     
     private func setupView() {
-        self.backgroundColor = UIColor(named: "ButtonColor")
+        self.backgroundColor = Constants.Color.mainBackgroud.color
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func addSubviews() {
+        self.addSubview(mainLabel)
+        self.addSubview(tempLogo)
+        self.addSubview(citation)
         self.addSubview(loginButton)
+        self.addSubview(registrationButton)
+        self.addSubview(termsLabel)
+        self.addSubview(delimiter)
+        self.addSubview(privacyLabel)
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            loginButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            loginButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            mainLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 30),
+            mainLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            tempLogo.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 20),
+            tempLogo.widthAnchor.constraint(equalToConstant: 200),
+            tempLogo.heightAnchor.constraint(equalToConstant: 200),
+            tempLogo.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            citation.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            citation.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
             loginButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            loginButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+            loginButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            loginButton.bottomAnchor.constraint(equalTo: registrationButton.topAnchor, constant: -20),
+            
+            registrationButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            registrationButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            registrationButton.bottomAnchor.constraint(equalTo: termsLabel.topAnchor, constant: -20),
+            
+            delimiter.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            delimiter.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            termsLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            termsLabel.trailingAnchor.constraint(equalTo: delimiter.leadingAnchor, constant: -5),
+            
+            privacyLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            privacyLabel.leadingAnchor.constraint(equalTo: delimiter.trailingAnchor, constant: 5)
         ])
     }
     
@@ -50,7 +215,11 @@ final class IntroductionView: UIView {
             })
         })
         group.notify(queue: .main) {
-            self.presenter?.login(user)
+            if sender.tag == 1 {
+                self.presenter?.login(user)
+            } else {
+                self.presenter?.register(user)
+            }
         }
     }
 }
